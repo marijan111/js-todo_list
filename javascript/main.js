4bd05e44e75750b968c45f2fd60635a771b1701d
marijan111 <marijanbao@gmail.com>
2020-04-09 23:38:15 +0200

    up

--- javascript/main.js
+++ javascript/main.js
@@ -1,17 +1,21 @@
 let newInputTodo = '';
-let todo1 = '';
+let todoItem = document.getElementsByClassName('todo-item');
 
+//When you write something in input it gets saved as variable newInputTodo
 function newInput() {
   newInputTodo = document.getElementById('newTodo').value;
 }
 
+//On Add To Your List button click this function creates a new list element
 function addTodo() {
+  let list = document.getElementById('todo-list');
   let li = document.createElement('li');
-  li.innerHTML = todo1;
-  if (todo1 === '') {
-    todo1 = newInputTodo;
-  } else {
-    console.log('You already added that item to your todo list.');
-  }
-  document.body.appendChild(li);
+  let removeBtn = document.createElement('i');
+
+  li.appendChild(document.createTextNode(newInputTodo));
+  li.setAttribute('class', 'list-group-item todo-item');
+  removeBtn.setAttribute('class', 'fas fa-trash-alt remove-btn');
+  removeBtn.addEventListener('click', removeItem);
+  li.appendChild(removeBtn);
+  list.appendChild(li);
 }
