0204cfd6c41c0640b5144b4fa37abeeeb62a3dee
marijan111 <marijanbao@gmail.com>
2020-04-10 21:08:36 +0200

    update

--- javascript/main.js
+++ javascript/main.js
@@ -1,21 +1,25 @@
 let newInputTodo = '';
 let todoItem = document.getElementsByClassName('todo-item');
+let list = document.getElementById('todo-list');
+let removeBtn = document.getElementsByClassName('remove-btn');
 
-//When you write something in input it gets saved as variable newInputTodo
+// When you write something in input it gets saved as variable newInputTodo
 function newInput() {
   newInputTodo = document.getElementById('newTodo').value;
 }
 
-//On Add To Your List button click this function creates a new list element
+// On Add To Your List button click this function creates a new list element
 function addTodo() {
-  let list = document.getElementById('todo-list');
-  let li = document.createElement('li');
-  let removeBtn = document.createElement('i');
-
-  li.appendChild(document.createTextNode(newInputTodo));
-  li.setAttribute('class', 'list-group-item todo-item');
-  removeBtn.setAttribute('class', 'fas fa-trash-alt remove-btn');
-  removeBtn.addEventListener('click', removeItem);
-  li.appendChild(removeBtn);
-  list.appendChild(li);
+  if (newInputTodo !== '') {
+    // creates new li element
+    let li = document.createElement('li');
+    // adds class attribute to that li element
+    li.classList.add('list-group-item', 'todo-item');
+    // writes some html inside that li element
+    li.innerHTML = `${newInputTodo}<i class="fas fa-trash-alt remove-btn" onclick="removeItem()"></i>`;
+    // append that li element to ul list with id todo-list
+    list.appendChild(li);
+  }
 }
+
+function removeItem() {}
